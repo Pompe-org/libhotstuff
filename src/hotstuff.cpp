0f17daf588090a9943091064782aa896ef296617
Yunhao <yz2327@cornell.edu>
2023-03-22 00:07:43 -0400

    debug

--- src/hotstuff.cpp
+++ src/hotstuff.cpp
@@ -691,8 +691,13 @@ void HotStuffBase::start(
                     cmds.push_back(cmd_pending_buffer.front());
                     cmd_pending_buffer.pop();
                 }
+
+                if (proposer == get_id())
+                    printf("[DEBUG] proposer before is %d\n", proposer);
+
                 pmaker->beat().then([this, cmds = std::move(cmds)](ReplicaID proposer) {
-                    printf("[DEBUG] proposer is %d\n", proposer);
+                    if (proposer == get_id())
+                        printf("[DEBUG] proposer after is %d\n", proposer);
                     if (proposer == get_id())
                         on_propose(cmds, pmaker->get_parents());
                 });
