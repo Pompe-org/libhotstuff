b995e476fd7880ba327f3f6471fa6218a48c4b4c
Yunhao <yz2327@cornell.edu>
2023-03-21 22:49:21 -0400

    debug

--- examples/pompe_app.cpp
+++ examples/pompe_app.cpp
@@ -234,7 +234,7 @@ int main(int argc, char **argv) {
     auto opt_fixed_proposer = Config::OptValInt::create(1);
     auto opt_base_timeout = Config::OptValDouble::create(1000);
     auto opt_prop_delay = Config::OptValDouble::create(1000);
-    auto opt_imp_timeout = Config::OptValDouble::create(1000);
+    auto opt_imp_timeout = Config::OptValDouble::create(1);
     auto opt_nworker = Config::OptValInt::create(1);
     auto opt_repnworker = Config::OptValInt::create(1);
     auto opt_repburst = Config::OptValInt::create(1000000);
