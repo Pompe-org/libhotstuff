87524d010448ce2a6258408bf328791f7c9af864
Yunhao <yz2327@cornell.edu>
2023-03-22 00:35:15 -0400

    debug

--- src/hotstuff.cpp
+++ src/hotstuff.cpp
@@ -629,6 +629,8 @@ void HotStuffBase::start(
                  //     s4 << cmd_hash4;
                  //     printf("[DEBUG] consensus#%d, %d->%d, 0x%s, %s, %s, %s\n", debug_invoked++, start, end, s1.get_hex().c_str(), s2.get_hex().c_str(), s3.get_hex().c_str(), s4.get_hex().c_str());
                  // }
+                 printf("[DEBUG] consensus %d start\n", debug_invoked++);
+
                  exec_command(commit_set_hash, [this, e, commit_set_hash](Finality fin) {
                      uint32_t start = exec_client_rsp[commit_set_hash].first;
                      uint32_t end = exec_client_rsp[commit_set_hash].second;
@@ -642,21 +644,20 @@ void HotStuffBase::start(
                      if (exec_count < end)
                          exec_count = end;
                  });
-                 usleep(2000);
-                 printf("[DEBUG] consensus %d start\n", debug_invoked++);
+                 
 
                  // place-holder cmd2
                  //exec_command_noresp(cmd_hash2);
                  //cmd_noresp_pending.enqueue(cmd_hash2);
-                 exec_command(cmd_hash2, [this](Finality fin) {});
+                 exec_command(cmd_hash2, [this](Finality fin) { printf("[DEBUG] consensus %d finalized\n", fin.cmd_height); });
                  // place-holder cmd3
                  //exec_command_noresp(cmd_hash3);
                  //cmd_noresp_pending.enqueue(cmd_hash3);
-                 exec_command(cmd_hash3, [this](Finality fin) {});
+                 exec_command(cmd_hash3, [this](Finality fin) { printf("[DEBUG] consensus %d finalized\n", fin.cmd_height); });
                  // place-holder cmd4
                  //exec_command_noresp(cmd_hash4);
                  //cmd_noresp_pending.enqueue(cmd_hash4);
-                 exec_command(cmd_hash4, [this](Finality fin) {});
+                 exec_command(cmd_hash4, [this](Finality fin) { printf("[DEBUG] consensus %d finalized\n", fin.cmd_height); });
 
                  return true;
              }
