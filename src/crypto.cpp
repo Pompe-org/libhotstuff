eb05ee6f8fde71b86c6ee69972aa2e1e8b014b88
Yunhao <yz2327@cornell.edu>
2023-03-21 22:30:36 -0400

    debug

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -58,7 +58,10 @@ promise_t QuorumCertSecp256k1::verify(const ReplicaConfig &config, VeriPool &vpo
         }
     return promise::all(vpm).then([](const promise::values_t &values) {
         for (const auto &v: values)
-            if (!promise::any_cast<bool>(v)) return false;
+            if (!promise::any_cast<bool>(v)) {
+                HOTSTUFF_LOG_WARN("src/crypto.cpp verify() failed");
+                return false;
+            }
         return true;
     });
 }
