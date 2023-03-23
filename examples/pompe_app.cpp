1243e3f73afb81fb6fb298352b9908fd415f14cc
Yunhao <yz2327@cornell.edu>
2023-03-23 12:56:20 -0400

    minor

--- examples/pompe_app.cpp
+++ examples/pompe_app.cpp
@@ -372,7 +372,7 @@ int main(int argc, char **argv) {
     papp->start(reps);
     elapsed.stop(true);
 
-    printf("server%d write to log file %s\n", idx, logfile.c_str());
+    printf("[DEBUG] server%d write to log file %s\n", idx, logfile.c_str());
     if (debug_timer_trigger > 0)
         printf("[DEBUG] server%d finished %d ordering phases; timer triggered %d times; send %d exec responses to clients with %d callbacks\n", idx, debug_finish_ordering_phase, debug_timer_trigger, debug_server_exec_resp, debug_timer_callback_trigger);
     freopen(logfile.c_str(), "w", stdout);
