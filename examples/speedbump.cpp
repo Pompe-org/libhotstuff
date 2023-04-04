d540a079ed88ad1b0d7e451b6bd9f96a5a9c0105
Yunhao <yz2327@cornell.edu>
2023-04-04 16:13:51 -0400

    more speedbump

--- examples/speedbump.cpp
+++ examples/speedbump.cpp
@@ -157,11 +157,10 @@ int main(int argc, char **argv) {
     }
     if (!(0 <= idx && (size_t)idx < raw.size() && raw.size() > 0))
         throw std::invalid_argument("out of range");
-    for (const auto &p: raw)
     {
-        auto _p = split_ip_port_cport(p);
+        auto _p = split_ip_port_cport(raw[idx]);
         size_t _;
-        printf("p.first: %s, p.second: %s\n", _p.first.c_str(), _p.second.c_str());
+        printf("Bump#%d connects to %s, %s\n", idx, _p.first.c_str(), _p.second.c_str());
         nodes.push_back(NetAddr(NetAddr(_p.first).ip, htons(stoi(_p.second, &_))));
     }
 
