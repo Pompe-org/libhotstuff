4375796fa7db15189977e68ddbde07f945430d96
Yunhao <yz2327@cornell.edu>
2023-04-06 15:09:24 -0400

    debug

--- examples/pompe_client.cpp
+++ examples/pompe_client.cpp
@@ -320,9 +320,9 @@ int main(int argc, char **argv) {
     for (auto it : finished) {
         int64_t invocation = it.second.invocation_time_us;
         std::sort(it.second.timestamps.begin(), it.second.timestamps.end());
+        printf("\n######################\n");
         for (auto t : it.second.timestamps)
-            printf("%ld = %ld - %ld\t", (int64_t)t - invocation, t, invocation);
-        printf("\n");
+            printf("    %ld (%ld - %ld)\t", (int64_t)t - invocation, t, invocation);
         if (print_total++ > 10) break;
     }
     
