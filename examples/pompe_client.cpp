bedb97ab302c17914bb10fd0b8262c8c60ddba7b
Yunhao <yz2327@cornell.edu>
2023-04-06 21:34:54 -0400

    debug

--- examples/pompe_client.cpp
+++ examples/pompe_client.cpp
@@ -317,7 +317,7 @@ int main(int argc, char **argv) {
     //printf("client write to exec log file %s, %lu entries\n", execlogfile.c_str(), elapsed_exec.size());
     printf("[DEBUG] client%d receives %d ordering, %d consensus responses\n", cid, elapsed.size(), count_exec);
 
-    int finished_len = finished.size() * 9 / 10;
+    int finished_len = 100; // Get statistics of the first 100 invocations
     std::vector<int64_t> results(4); // Assume 4 nodes
     for (int i = 0; i < finished_len; i++) {
         int64_t invocation = finished[i].invocation_time_us;
