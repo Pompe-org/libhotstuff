7e2f43baf7f8821820146ee92dd8f8cdf46b32ae
Yunhao <yz2327@cornell.edu>
2023-04-08 20:30:40 -0400

    debug

--- examples/pompe_unbias_2clients.cpp
+++ examples/pompe_unbias_2clients.cpp
@@ -349,9 +349,9 @@ int main(int argc, char **argv) {
 #ifdef HOTSTUFF_ENABLE_BENCHMARK
 
     printf("client write to order log file %s, %lu entries\n", orderlogfile.c_str(), elapsed.size());
+    return 0;
     //printf("client write to exec log file %s, %lu entries\n", execlogfile.c_str(), elapsed_exec.size());
     printf("[DEBUG] client%d receives %d ordering, %d consensus responses\n", cid, elapsed.size(), count_exec);
-    return 0;
 
     int finished_len = 100; // Get statistics of the first 100 invocations
     std::vector<int64_t> invoke_to_recv(4); // Assume 4 nodes
