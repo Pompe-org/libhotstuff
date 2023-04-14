e46f101ceafa54e7144d92a2a9eac1962743e76e
Yunhao Zhang <yz2327@cornell.edu>
2023-04-14 19:06:24 -0400

    debug

--- examples/large_pompe_unbias_2clients.cpp
+++ examples/large_pompe_unbias_2clients.cpp
@@ -132,6 +132,7 @@ bool try_send(bool check = true) {
         // Weak client's command
         auto cmd0 = new CommandDummy(cid, cnt++);
         //MsgOrdering1ReqCmd msg0(*cmd0);
+        printf("[TMP] client sends EstConnReq\n");
         MsgEstConnReqCmd msg0(*cmd0);
         for (int i = 0; i < BATCH_SIZE; i++) {
             for (auto &p: weak_conns) mn.send_msg(msg0, p.second);
@@ -146,6 +147,7 @@ bool try_send(bool check = true) {
             for (auto &p: strong_conns) mn.send_msg(msg1, p.second);
         }
         waiting.insert(std::make_pair(cmd1->get_hash(), Request(cmd1, cmd0, true)));
+        printf("[TMP] client finishes sending EstConnReq\n");
 
 #ifndef HOTSTUFF_ENABLE_BENCHMARK
         HOTSTUFF_LOG_INFO("send new cmd %.10s",
@@ -159,6 +161,8 @@ bool try_send(bool check = true) {
 }
 
 void client_estconn_resp_cmd_handler(MsgEstConnRespCmd &&msg, const Net::conn_t &conn) {
+    printf("[TMP] client receives EstConnResp\n");
+
     const uint256_t &cmd_hash = msg.cmd_hash;
     auto it = waiting.find(cmd_hash);
     if (it == waiting.end()) return;
