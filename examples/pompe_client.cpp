61824a30af8eb7d14ede6eb25797ef60872d06b0
Yunhao <Yunhao@localhost>
2024-04-14 14:44:08 -0600

    minor

--- examples/pompe_client.cpp
+++ examples/pompe_client.cpp
@@ -245,7 +245,7 @@ int main(int argc, char **argv) {
     auto opt_idx = Config::OptValInt::create(0);
     auto opt_replicas = Config::OptValStrVec::create();
     auto opt_max_iter_num = Config::OptValInt::create(-1);
-    auto opt_max_async_num = Config::OptValInt::create(10);
+    auto opt_max_async_num = Config::OptValInt::create(4);
     auto opt_cid = Config::OptValInt::create(-1);
 
     auto shutdown = [&](int) { ec.stop(); };
