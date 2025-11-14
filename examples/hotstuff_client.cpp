25fe14aed54b55c8e96b725742f8e19c3b4011b4
Yunhao Zhang <Yunhao@control.yunhao-279384.defibench.emulab.net>
2025-11-14 15:56:30 -0700

    Fix minor issue

--- examples/hotstuff_client.cpp
+++ examples/hotstuff_client.cpp
@@ -169,7 +169,7 @@ int main(int argc, char **argv) {
     while (try_send());
     ec.dispatch();
 
-    printf("client%d executed %lu commands\n", idx, elapsed.size());
+    printf("client%d executed %lu commands\n", cid, elapsed.size());
 
 #ifdef HOTSTUFF_ENABLE_BENCHMARK
     for (const auto &e: elapsed)
